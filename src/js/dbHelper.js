004c0d9d2cfabea50f40a2fae7a0213f3db42484
jiyoonOh <jiyoonOh.dev@gmail.com>
2021-08-11 21:07:24 +0900

    Revert "Use MYSQL, Code Refactoring"
    This reverts commit 282b622f79af02531ef93ec44d41600f3b48b24a.

--- src/js/dbHelper.js
+++ src/js/dbHelper.js
@@ -47,16 +47,6 @@ class DbHelper{
         this.save();
     }
 
-    /** 
-     * itemText수정
-     * @param {string} itemId 
-     * @param {string} editText 
-     */
-    updateText(itemId, editText){
-        this.#dataList.filter(f=>f.id==itemId).forEach(it=>{it.text = editText;});
-        this.save();
-    }
-
     /**
      * itemId 에 해당하는 item 삭제 
      * @param {string} itemId 
@@ -65,5 +55,4 @@ class DbHelper{
         this.#dataList = this.#dataList.filter(f=>f.id!=itemId);
         this.save();
     }
-    
 }
